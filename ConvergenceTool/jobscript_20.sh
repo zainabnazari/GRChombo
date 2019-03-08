c68569ef8b3ed01664f989c80d8aa70ec34cd296
fmuia <muia2987@gmail.com>
2019-03-08 15:24:00 +0100

    Convergence Not Working

--- ConvergenceTool/jobscript_20.sh
+++ ConvergenceTool/jobscript_20.sh
@@ -11,4 +11,4 @@ module load gcc/4.9.0 openmpi/1.10.2/intel/2016 intel/2016 gnu-openmpi/phdf5/1.8
 
 # add here you command line to run a parallel job
 export OMP_NUM_THREADS=1
-mpirun ./ConvergenceTool3d.Linux.64.mpicxx.ifort.OPTHIGH.MPI.OPENMPCC.ex params.txt
+mpirun ./ConvergenceTool3d.Linux.64.mpicxx.ifort.DEBUG.OPT.MPI.OPENMPCC.ex params.txt
