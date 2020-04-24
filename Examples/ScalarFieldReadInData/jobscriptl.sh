6873f1aaa42bdbe2d4f1369777de6f65007bc1a7
Zainab Nazari <znazari0@r000u07l02.marconi.cineca.it>
2020-04-24 14:46:11 +0200

    jobscriptl modified

--- Examples/ScalarFieldReadInData/jobscriptl.sh
+++ Examples/ScalarFieldReadInData/jobscriptl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=2
 #SBATCH --partition=skl_usr_prod
 #SBATCH --mem=180000
 #SBATCH -A ict20_hep
-#SBATCH --time=24:00:00
+#SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=2
 # add here you command line to run a parallel job
 export OMP_NUM_THREADS=2
